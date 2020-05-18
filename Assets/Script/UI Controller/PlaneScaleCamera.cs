a0e33ff74435d605066ebfd5a8b0f6ee2710dc48
rattasart146 <59070146@it.kmitl.ac.th>
2020-05-19 04:01:06 +0700

    Add Blueprint Function

---
+++ Assets/Script/UI Controller/PlaneScaleCamera.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlaneScaleCamera : MonoBehaviour
+{
+    public Camera resultCam;
+    public GameObject areaMesh;
+    private Bounds objectSize;
+    int i = 0;
+
+    void Start()
+    {
+    }
+    void Update()
+    {
+        if (areaMesh != null)
+        {
+            objectSize = areaMesh.GetComponent<MeshFilter>().mesh.bounds;
+
+            float screenRatio = (float)Screen.width / (float)Screen.height;
+            float targetRatio = objectSize.size.x / objectSize.size.z;
+
+            if (screenRatio >= targetRatio)
+           {
+                resultCam.orthographicSize = objectSize.size.z / 2;
+            }
+            else
+            {
+                float differenceInSize = targetRatio / screenRatio;
+                resultCam.orthographicSize = objectSize.size.z / 2 * differenceInSize;
+            }
+
+            resultCam.transform.position = new Vector3(objectSize.center.x, resultCam.transform.position.y, objectSize.center.z);
+        }
+    }
+}
